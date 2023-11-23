0f7dcf878376cbace6666eaf7101c3fd1a8ac560
instafever <495140158@qq.com>
2023-11-24 02:00:50 +0800

    mouse

--- app/todo/components/Item/index.js
+++ app/todo/components/Item/index.js
@@ -1,1 +1,21 @@
-export default function Item() {}
+"use client";
+
+import { useState } from "react";
+
+// 列表项
+export default function Item() {
+  // 记录鼠标移入
+  const { mouse, setMouse } = useState(false);
+  // 接收参数，返回函数用于事件绑定
+  handleMouse = (flag) => () => setMouse(flag);
+
+  return (
+    <li
+      style={{ backgroundColor: mouse ? "#ddd" : "white" }}
+      onMouseEnter={handleMouse(true)}
+      onMouseLeave={handleMouse(false)}
+    >
+      <button style={{ display: mouse ? "block" : "none" }}></button>
+    </li>
+  );
+}
