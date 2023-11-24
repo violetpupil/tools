25e8ffa5392a8a3815c705eb62d9341f97f74ac8
shouguang <495140158@qq.com>
2023-11-24 12:48:22 +0800

    视频分段规则

--- engine/kernel/show.go
+++ engine/kernel/show.go
@@ -5,4 +5,5 @@ type Show struct {
 	Platform     string `json:"platform"`      // 网站id
 	RoomID       string `json:"room_id"`       // 房间id
 	StreamerName string `json:"streamer_name"` // 网站名
+	SplitRule    string `json:"split_rule"`    // 视频分段规则
 }
