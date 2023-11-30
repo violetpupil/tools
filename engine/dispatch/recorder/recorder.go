4caace2695db3131939c5d2b3e54d6e9788ae9db
shouguang <495140158@qq.com>
2023-11-30 15:57:41 +0800

    record

--- engine/dispatch/recorder/recorder.go
+++ engine/dispatch/recorder/recorder.go
@@ -1,1 +1,30 @@
 package recorder
+
+import (
+	"olive/engine/dispatch/common"
+
+	"github.com/sirupsen/logrus"
+)
+
+type recorder struct {
+	bout   common.Bout
+	log    *logrus.Logger
+	status common.StatusID
+	stop   chan struct{}
+	done   chan struct{}
+}
+
+func newRecorder(log *logrus.Logger, bout common.Bout) (*recorder, error) {
+	return &recorder{
+		status: common.Status.Starting,
+		bout:   bout,
+		log:    log,
+		stop:   make(chan struct{}),
+		done:   make(chan struct{}),
+	}, nil
+}
+
+func (r *recorder) record() error {
+	// TODO
+	return nil
+}
