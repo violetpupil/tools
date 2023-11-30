75ba71bd989f34dc3defe8c9c9b35dbdc09ea61e
shouguang <495140158@qq.com>
2023-11-30 16:12:28 +0800

    Manager

---
+++ engine/parser/manager.go
@@ -0,0 +1,31 @@
+package parser
+
+import "olive/engine/log"
+
+var SharedManager = &Manager{}
+
+type Manager struct {
+	savers map[string]Parser
+}
+
+func (p *Manager) Register(parsers ...Parser) {
+	if p.savers == nil {
+		p.savers = map[string]Parser{}
+	}
+	for _, parser := range parsers {
+		_, ok := p.savers[parser.Type()]
+		if ok {
+			log.Logger.Warnf("parser %s has registered", parser.Type())
+		}
+		p.savers[parser.Type()] = parser
+	}
+}
+
+func (p *Manager) Parser(typ string) (Parser, bool) {
+	v, ok := p.savers[typ]
+	return v, ok
+}
+
+type Parser interface {
+	Type() string
+}
