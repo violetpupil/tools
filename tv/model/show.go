cf746d65f148e4a3eafb15cb0724e60d4ccb7b50
shouguang <495140158@qq.com>
2023-12-04 15:43:55 +0800

    tv

---
+++ tv/model/show.go
@@ -0,0 +1,7 @@
+package model
+
+// Show 直播活动
+type Show struct {
+	RoomURL   string
+	StreamURL string
+}
---
+++ tv/site/douyin.go
@@ -0,0 +1,8 @@
+package site
+
+import "olive/tv/model"
+
+type douyin struct{}
+
+// Snap 抓取直播信息
+func (s *douyin) Snap(show *model.Show) {}
