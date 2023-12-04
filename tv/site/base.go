9087dc560008ceb0d8c30d80f9dc1516aaf35e1a
shouguang <495140158@qq.com>
2023-12-04 16:36:23 +0800

    SimplifyRoomURL

--- tv/site/base.go
+++ tv/site/base.go
@@ -1,9 +1,21 @@
 package site
 
-import "github.com/violetpupil/gos/std/strings"
+import (
+	"net/url"
+	"olive/tv/model"
+
+	"github.com/violetpupil/gos/std/strings"
+)
 
 type base struct{}
 
-func (b *base) RoomID(roomURL string) string {
-	return strings.SplitLast(roomURL, "/")
+// SetRoomID 解析直播间 id 并设置
+func (b *base) SetRoomID(show *model.Show) error {
+	u, err := url.Parse(show.URL)
+	if err != nil {
+		return err
+	}
+	path := strings.TrimSuffix(u.Path, "/")
+	show.ID = strings.SplitLast(path, "/")
+	return nil
 }
--- tv/site/douyin.go
+++ tv/site/douyin.go
@@ -6,5 +6,15 @@ type douyin struct {
 	*base
 }
 
+// SimplifyRoomURL 简化直播间地址
+func (s *douyin) SimplifyRoomURL(show *model.Show) error {
+	err := s.SetRoomID(show)
+	if err != nil {
+		return err
+	}
+	show.URL = "https://live.douyin.com/" + show.ID
+	return nil
+}
+
 // Snap 抓取直播信息
 func (s *douyin) Snap(show *model.Show) {}
